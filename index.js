21323958ec1e23ca95ad089503a77898c65f1244
Luzia <89281588+luziasodev@users.noreply.github.com>
2022-09-14 11:47:06 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,81 @@
+function amyFunction() {
+    var adots = document.getElementById("adots");
+    var amoreText = document.getElementById("amore");
+    var abtnText = document.getElementById("amyBtn");
+  
+    if (adots.style.display === "none") {
+      adots.style.display = "inline";
+      abtnText.innerHTML = "...";
+      amoreText.style.display = "none";
+    } else {
+      adots.style.display = "none";
+      abtnText.innerHTML = "ocultar";
+      amoreText.style.display = "inline";
+    }
+  }
+
+  function emyFunction() {
+    var edots = document.getElementById("edots");
+    var emoreText = document.getElementById("emore");
+    var ebtnText = document.getElementById("emyBtn");
+  
+    if (edots.style.display === "none") {
+      edots.style.display = "inline";
+      ebtnText.innerHTML = "...";
+      emoreText.style.display = "none";
+    } else {
+      edots.style.display = "none";
+      ebtnText.innerHTML = "ocultar";
+      emoreText.style.display = "inline";
+    }
+  }
+
+  function pmyFunction() {
+    var pdots = document.getElementById("pdots");
+    var pmoreText = document.getElementById("pmore");
+    var pbtnText = document.getElementById("pmyBtn");
+  
+    if (pdots.style.display === "none") {
+      pdots.style.display = "inline";
+      pbtnText.innerHTML = "...";
+      pmoreText.style.display = "none";
+    } else {
+      pdots.style.display = "none";
+      pbtnText.innerHTML = "ocultar";
+      pmoreText.style.display = "inline";
+    }
+  }
+
+  function smyFunction() {
+    var pdots = document.getElementById("sdots");
+    var pmoreText = document.getElementById("smore");
+    var pbtnText = document.getElementById("smyBtn");
+  
+    if (pdots.style.display === "none") {
+      pdots.style.display = "inline";
+      pbtnText.innerHTML = "...";
+      pmoreText.style.display = "none";
+    } else {
+      pdots.style.display = "none";
+      pbtnText.innerHTML = "ocultar";
+      pmoreText.style.display = "inline";
+    }
+  }
+
+  function tmyFunction() {
+    var tdots = document.getElementById("tdots");
+    var tmoreText = document.getElementById("tmore");
+    var tbtnText = document.getElementById("tmyBtn");
+  
+    if (tdots.style.display === "none") {
+      tdots.style.display = "inline";
+      tbtnText.innerHTML = "...";
+      tmoreText.style.display = "none";
+    } else {
+      tdots.style.display = "none";
+      tbtnText.innerHTML = "ocultar";
+      tmoreText.style.display = "inline";
+    }
+  }
+
+  
