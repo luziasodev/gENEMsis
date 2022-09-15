579da51d072f064948b6844607a8c0b97ee8a188
Gabriel Pereira <gabrielpmonte@hotmail.com>
2022-09-15 17:54:02 -0300

    style: improve readability

--- index.js
+++ index.js
@@ -1,81 +1,80 @@
 function amyFunction() {
-    var adots = document.getElementById("adots");
-    var amoreText = document.getElementById("amore");
-    var abtnText = document.getElementById("amyBtn");
-  
-    if (adots.style.display === "none") {
-      adots.style.display = "inline";
-      abtnText.innerHTML = "...";
-      amoreText.style.display = "none";
-    } else {
-      adots.style.display = "none";
-      abtnText.innerHTML = "ocultar";
-      amoreText.style.display = "inline";
-    }
-  }
+  const adots = document.getElementById("adots");
+  const amoreText = document.getElementById("amore");
+  const abtnText = document.getElementById("amyBtn");
 
-  function emyFunction() {
-    var edots = document.getElementById("edots");
-    var emoreText = document.getElementById("emore");
-    var ebtnText = document.getElementById("emyBtn");
-  
-    if (edots.style.display === "none") {
-      edots.style.display = "inline";
-      ebtnText.innerHTML = "...";
-      emoreText.style.display = "none";
-    } else {
-      edots.style.display = "none";
-      ebtnText.innerHTML = "ocultar";
-      emoreText.style.display = "inline";
-    }
+  if (adots.style.display === "none") {
+    adots.style.display = "inline";
+    abtnText.innerHTML = "...";
+    amoreText.style.display = "none";
+  } else {
+    adots.style.display = "none";
+    abtnText.innerHTML = "ocultar";
+    amoreText.style.display = "inline";
   }
+}
 
-  function pmyFunction() {
-    var pdots = document.getElementById("pdots");
-    var pmoreText = document.getElementById("pmore");
-    var pbtnText = document.getElementById("pmyBtn");
-  
-    if (pdots.style.display === "none") {
-      pdots.style.display = "inline";
-      pbtnText.innerHTML = "...";
-      pmoreText.style.display = "none";
-    } else {
-      pdots.style.display = "none";
-      pbtnText.innerHTML = "ocultar";
-      pmoreText.style.display = "inline";
-    }
+function emyFunction() {
+  const edots = document.getElementById("edots");
+  const emoreText = document.getElementById("emore");
+  const ebtnText = document.getElementById("emyBtn");
+
+  if (edots.style.display === "none") {
+    edots.style.display = "inline";
+    ebtnText.innerHTML = "...";
+    emoreText.style.display = "none";
+  } else {
+    edots.style.display = "none";
+    ebtnText.innerHTML = "ocultar";
+    emoreText.style.display = "inline";
   }
+}
 
-  function smyFunction() {
-    var pdots = document.getElementById("sdots");
-    var pmoreText = document.getElementById("smore");
-    var pbtnText = document.getElementById("smyBtn");
-  
-    if (pdots.style.display === "none") {
-      pdots.style.display = "inline";
-      pbtnText.innerHTML = "...";
-      pmoreText.style.display = "none";
-    } else {
-      pdots.style.display = "none";
-      pbtnText.innerHTML = "ocultar";
-      pmoreText.style.display = "inline";
-    }
+function pmyFunction() {
+  const pdots = document.getElementById("pdots");
+  const pmoreText = document.getElementById("pmore");
+  const pbtnText = document.getElementById("pmyBtn");
+
+  if (pdots.style.display === "none") {
+    pdots.style.display = "inline";
+    pbtnText.innerHTML = "...";
+    pmoreText.style.display = "none";
+  } else {
+    pdots.style.display = "none";
+    pbtnText.innerHTML = "ocultar";
+    pmoreText.style.display = "inline";
   }
+}
 
-  function tmyFunction() {
-    var tdots = document.getElementById("tdots");
-    var tmoreText = document.getElementById("tmore");
-    var tbtnText = document.getElementById("tmyBtn");
-  
-    if (tdots.style.display === "none") {
-      tdots.style.display = "inline";
-      tbtnText.innerHTML = "...";
-      tmoreText.style.display = "none";
-    } else {
-      tdots.style.display = "none";
-      tbtnText.innerHTML = "ocultar";
-      tmoreText.style.display = "inline";
-    }
+function smyFunction() {
+  const pdots = document.getElementById("sdots");
+  const pmoreText = document.getElementById("smore");
+  const pbtnText = document.getElementById("smyBtn");
+
+  if (pdots.style.display === "none") {
+    pdots.style.display = "inline";
+    pbtnText.innerHTML = "...";
+    pmoreText.style.display = "none";
+  } else {
+    pdots.style.display = "none";
+    pbtnText.innerHTML = "ocultar";
+    pmoreText.style.display = "inline";
   }
+}
 
-  
+function tmyFunction() {
+  const tdots = document.getElementById("tdots");
+  const tmoreText = document.getElementById("tmore");
+  const tbtnText = document.getElementById("tmyBtn");
+
+  if (tdots.style.display === "none") {
+    tdots.style.display = "inline";
+    tbtnText.innerHTML = "...";
+    tmoreText.style.display = "none";
+  } else {
+    tdots.style.display = "none";
+    tbtnText.innerHTML = "ocultar";
+    tmoreText.style.display = "inline";
+  }
+}
+
